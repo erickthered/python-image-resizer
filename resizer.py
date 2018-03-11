8530d4cd78601dee356a4904be1785b1d95aea5e
Erick Rojas <rojas_erick@hotmail.com>
2018-03-11 12:58:20 +0000

    Now it works with all files in a directory or just one file

--- resizer.py
+++ resizer.py
@@ -4,40 +4,43 @@ import os
 import sys
 from PIL import Image
 
+
 class Resizer():
-    def __init__(self, directory):
-        self.directory = directory
-        self.files = []
-
-    def getFiles(self):
-        self.files = [item for item in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, item))]
-
-    def getNewSize(self, oldsize, width = 0, height = 0):
-        if (width > 0):
-            height = int(width/oldsize[0] * oldsize[1])
+    def __init__(self, path):
+        if not os.path.isfile(path):
+            self.files = [os.path.join(path, item) for item in os.listdir(path) if os.path.isfile(os.path.join(path, item))]
         else:
-            width = int(height/oldsize[1] * oldsize[0])
+            self.files = [path]
+
+    def getNewSize(self, oldsize, width=0, height=0):
+        if (width > 0):
+            height = int(width / oldsize[0] * oldsize[1])
+        else:
+            width = int(height / oldsize[1] * oldsize[0])
         return (width, height)
 
-    def process(self, maxwidth = 0, maxheight = 0):
-        self.getFiles()
+    def resizeFile(self, file, maxwidth=0, maxheight=0):
+        image = Image.open(file)
+        if maxwidth > 0 and image.width > maxwidth:
+            newSize = self.getNewSize(image.size, width=maxwidth)
+            image = image.resize(newSize)
+            image.save(file)
+        elif maxheight > 0 and image.height > maxheight:
+            newSize = self.getNewSize(image.size, height=maxheight)
+            image = image.resize(newSize)
+            image.save(file)
+
+    def process(self, maxwidth=0, maxheight=0):
         for file in self.files:
             try:
-                image = Image.open(os.path.join(self.directory, file))
-                if maxwidth > 0 and image.width > maxwidth:
-                    newSize = self.getNewSize(image.size, width=maxwidth)
-                    image = image.resize(newSize)
-                    image.save(os.path.join(self.directory, file))
-                elif maxheight > 0 and image.height > maxheight:
-                    newSize = self.getNewSize(image.size, height=maxheight)
-                    image = image.resize(newSize)
-                    image.save(os.path.join(self.directory, file))
+                self.resizeFile(file, maxwidth, maxheight)
             except IOError:
                 pass
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        print(sys.argv[0], "<path_to_process> <width> <height>")
+        print(sys.argv[0], "<directory|file> <width> <height>")
     else:
         msg = "WARNING:\nThis program will overwrite any image files "
         msg += "that exceed the given size in pixels, please make sure "
@@ -48,4 +51,4 @@ if __name__ == "__main__":
             resizer = Resizer(sys.argv[1])
             resizer.process(maxwidth=int(sys.argv[2]), maxheight=int(sys.argv[3]))
         else:
-            print ("Nothing has been modified")
+            print("Nothing has been modified")
